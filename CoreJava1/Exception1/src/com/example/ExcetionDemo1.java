37e494cbb04d80081d3cedac4e323cd24713ea0d
anuja274 <divekar.anuja27@gmail.com>
2023-12-09 09:57:53 +0530

    new file added

---
+++ CoreJava1/Exception1/src/com/example/ExcetionDemo1.java
@@ -0,0 +1,46 @@
+package com.example;       // Exception(unchecked exception)-> java.lang package ParentClass->Throwable(partial checked)
+
+public class ExcetionDemo1 {
+
+	public static void main(String[] args) {
+		
+		/*System.out.println("statement1");
+		System.out.println(10/0);           //ArithmaticException->runtime exception occur
+		System.out.println("statement2");
+		*/
+		
+		/*************************************************************************************
+		statement1
+
+		Exception in thread "main" java.lang.ArithmeticException: / by zero
+			at com.example.ExcetionDemo1.main(ExcetionDemo1.java:8)
+		*/
+		
+		//************************************************************************************
+		
+		System.out.println("statement1");
+		
+		try 
+		{  
+			System.out.println(10/0);	 //ArithmaticException->runtime exception occur
+		}
+		 
+		catch(Exception e)
+		{
+			System.out.println(e);
+		}
+		
+		System.out.println("statement3");
+		
+		//***********************************************************************************
+		
+		/*statement1
+		java.lang.ArithmeticException: / by zero
+		statement3
+		*/
+       
+		//due to try-catch block prog normal flow not terminate abnormally
+		
+	}
+}
+
